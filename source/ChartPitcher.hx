4d0c33d2fb6bf430d51b4d733adcc78cae050344
Dominicthecodingenuis <101476659+Dominicthecodingenuis@users.noreply.github.com>
2022-09-15 23:09:52 -0700

    Create ChartPitcher.hx

---
+++ source/ChartPitcher.hx
@@ -0,0 +1,12 @@
+class ChartPitcher 
+{
+var num:Array<Int> = ['0.01,0.01'];
+    public function new()
+       {
+          num = CoolUtil.coolTextFile(Paths.txt('PitchInfo/num'));          
+       } 
+  
+  function control {
+    super.Override();
+  }
+}

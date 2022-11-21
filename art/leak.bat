227092058427e948396080f52eb060478de620d7
Dominicthecodingenuis <101476659+Dominicthecodingenuis@users.noreply.github.com>
2022-11-20 19:58:59 -0800

    sussybudussy

--- art/leak.bat
+++ art/leak.bat
@@ -1,3 +1,284 @@
-@echo off
+@ECHO OFF
 
-echo deez nutzz
+:choice
+set /P c=Would you like to download more leaks[Y/N]
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice
+
+
+:somewhere
+
+@echo off
+color 0a
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        Please Wait
+echo ---------------------------------------
+echo                                 =   0 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading.          Please Wait
+echo ---------------------------------------
+echo #                              =   5 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading..         Please Wait
+echo ---------------------------------------
+echo ##                            =  15 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        Please Wait
+echo ---------------------------------------
+echo ###                          =  23 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading.          Please Wait
+echo ---------------------------------------
+echo ####                        =  30 ]
+echo ---------------------------------------
+ping localhost -n 4 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading..         Please Wait
+echo ---------------------------------------
+echo #####                      =  38 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        Please Wait
+echo ---------------------------------------
+echo #####                      =  42 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading.          Please Wait
+echo ---------------------------------------
+echo ######                    =  45 ]
+echo ---------------------------------------
+ping localhost -n 1 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading..         Please Wait
+echo ---------------------------------------
+echo #######                  =  48 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        Please Wait
+echo ---------------------------------------
+echo ########                =  50 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading.           Please Wait
+echo ---------------------------------------
+echo #########              =  56 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading..         Please Wait
+echo ---------------------------------------
+echo ##########            =  63 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        Please Wait
+echo ---------------------------------------
+echo ###########          =  69 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading.          Please Wait
+echo ---------------------------------------
+echo ############        =  75 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading..         Please Wait
+echo ---------------------------------------
+echo #############      =  79 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        Please Wait
+echo ---------------------------------------
+echo ##############    =  86 ]
+echo ---------------------------------------
+ping localhost -n 2 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading.          Please Wait
+echo ---------------------------------------
+echo ###############  =  90 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading..         Please Wait
+echo ---------------------------------------
+echo ###############  =  96 ]
+echo ---------------------------------------
+ping localhost -n 3 >nul
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    Downloading...        .Complete.
+echo ---------------------------------------
+echo ################= 100 ]
+echo ---------------------------------------
+
+echo Files should be downloaded in
+
+pause
+
+:somewhere_else
+exit
